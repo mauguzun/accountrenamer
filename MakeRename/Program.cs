e8ea7d631dbaeb8e7f14b062c91bc39b9ac8c010
Denis <mauguzun@gmail.com>
2017-12-01 19:37:33 +0200

    not settings done

---
+++ MakeRename/Program.cs
@@ -0,0 +1,118 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeRename
+{
+    class Program
+    {
+        static ChromeDriver dr;
+        static Settings st;
+        static void Main(string[] args)
+        {
+            dr = new ChromeDriver();
+            dr.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 0, 35);
+
+
+            st = new Settings();
+
+
+            Console.WriteLine("set new settings?");
+            if(Console.ReadLine() == "y")
+            {
+               
+            }
+            string[] acc = File.ReadAllLines(st.Set["file"]);
+            var resut = acc;
+
+            for(int i = 0; i < resut.Count(); i++)
+            {
+                string newResult = SetNewName(resut[i]);
+                if (newResult != null)
+                {
+                    resut[i] = newResult;
+                    File.WriteAllLines(st.Set["result"], resut);
+                }
+            }
+
+
+
+        }
+
+
+        static string  SetNewName(string acc)
+        {
+         
+            try
+            {
+                string[] param = acc.Split(':');
+
+
+
+                if (param[2].Contains(st.Set["suffix"]))
+                    return null;
+
+
+                dr.Url = "https://www.pinterest.com/login/";
+                dr.FindElementByName("id").SendKeys(param[0]);
+                dr.FindElementByName("password").SendKeys(param[1]);
+                dr.FindElementByCssSelector("form > button").Click();
+
+                WebDriverWait wait = new WebDriverWait(dr, new TimeSpan(0, 0, 35));
+                wait.Until((d) => d.Url == "https://www.pinterest.com/");
+
+                dr.FindElementByCssSelector("body").Click();
+
+                dr.Url = "https://www.pinterest.com/settings";
+
+                string oldValue = dr.FindElementById("userUserName").GetAttribute("value");
+
+
+                
+
+                var newValue = $"{oldValue}{st.Set["suffix"]}";
+
+             
+                dr.FindElementById("userUserName").SendKeys(st.Set["suffix"]);
+                dr.FindElementById("userAbout").SendKeys("l");
+
+                if (dr.FindElementById("userUserName").GetAttribute("value") == newValue)
+                {
+                    var buttons = dr.FindElementsByCssSelector("form button[type = 'button']");
+                    foreach(var button in buttons)
+                    {
+                        if (button.Text == "Save settings")
+                        {
+                            button.Click();
+                            File.AppendAllText(st.Set["result_good"], newValue + Environment.NewLine);
+                            dr.Url = "https://www.pinterest.com/logout/";
+
+                            return $"{param[0]}:{param[1]}:{newValue}";
+                        }
+                           
+
+ 
+                    }
+
+                }
+                dr.Url = "https://www.pinterest.com/logout/";
+
+                return null;
+            }
+            catch(Exception ex)
+            {
+                dr.Url = "https://www.pinterest.com/logout/";
+
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+           
+          
+        }
+    }
+}
---
+++ MakeRename/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeRename
+{
+    public  class Settings
+    {
+
+        public Dictionary<string , string > Set { get; set; }
+
+        public Settings ()
+        {
+            Set = new Dictionary<string, string>();
+            Set.Add("file", @"C:\my_work_files\pinterest\source_all_account_for_blaster.txt");
+            Set.Add("result", @"C:\my_work_files\pinterest\source_all_account_for_blaster_renamed.txt");
+            Set.Add("result_bad", @"C:\my_work_files\pinterest\errors_rename.txt");
+            Set.Add("result_good", @"C:\my_work_files\pinterest\add_me.csv");
+            Set.Add("suffix", "_qf");
+           
+
+
+
+        }
+    }
+}
